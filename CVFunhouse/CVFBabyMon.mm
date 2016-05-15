93eea035ff066a04341f5928cdc635b26c3b3e04
John Brewer <jbrewer@jera.com>
2016-05-15 11:32:00 -0700

    Highlight areas above 35.6 C.

--- CVFunhouse/CVFBabyMon.mm
+++ CVFunhouse/CVFBabyMon.mm
@@ -10,11 +10,28 @@
 
 @implementation CVFBabyMon
 
+//const int FEVER_THRESH = 31115; // 100.4 degrees F in 100ths of a Kelvin.
+//const int FEVER_THRESH = 31015; // 98.6 degrees F in 100ths of a Kelvin.
+const int FEVER_THRESH = 30875; // 35.6 C (~96 F) (per NIH forehead study)
+
 using namespace cv;
 
--(void)processMat1:(cv::Mat)mat1 mat2:(cv::Mat)ycbcr
+-(void)processMat1:(cv::Mat)thermal mat2:(cv::Mat)ycbcr
 {
-#pragma unused(mat1)
+//    double min, max;
+//    cv::minMaxLoc(thermal, &min, &max);
+//    
+//    if (max > FEVER_THRESH) {
+//        
+//    }
+
+    MatIterator_<uint16_t> it, end;
+    for( it = thermal.begin<uint16_t>(), end = thermal.end<uint16_t>(); it != end; ++it) {
+        if (*it < FEVER_THRESH) {
+            *it = 0;
+        }
+    }
+    
     Mat ycrcb(ycbcr.rows, ycbcr.cols, CV_8UC3);
     
     int from_to[] = { 0,0, 1,2, 2,1 };
@@ -25,6 +42,17 @@ using namespace cv;
     Mat planes[3];
     split(rgbMat,planes);
     cvtColor(planes[0], rgbMat, CV_GRAY2RGB);
+    
+    Mat mask;
+    resize(thermal, mask, rgbMat.size());
+    
+    mask.convertTo(mask, CV_8U, 255.0 / 3885.0, -1858.5);
+    
+//    cvtColor(mask, rgbMat, CV_GRAY2RGB);
+
+    Mat red(rgbMat.rows, rgbMat.cols, CV_8UC3, Scalar(255, 0, 0));
+    
+    red.copyTo(rgbMat, mask);
     
     [self matReady:rgbMat];
 }
